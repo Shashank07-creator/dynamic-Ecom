0835db51db22319ec412f7daddfdf20e9c3b022b
Shashank <shashank_nigam@persistent.com>
2022-06-25 16:25:56 +0530

    Initial Commit

---
+++ dynamic-ecom/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(props) {
+    return (
+        <div className='header'>
+            <div>
+                <h1>Header Will Come Here</h1>
+            </div>
+        </div>
+    )
+}

cef1d3fb74bed09337019cd0caeec3eaa4c20dbf
Shashank <shashank_nigam@persistent.com>
2022-06-27 00:20:43 +0530

    Initiated dynamicness of Header

---
+++ dynamic-ecom/src/Components/Header/data.js
@@ -0,0 +1,19 @@
+let data = [
+    {
+        name:'Check anchor',
+        url:'/check',
+        viewType:'anchor'
+    },
+    {
+        name:'Image',
+        image:'/logo',
+        viewType:'image'
+    },
+    {
+        file:true,
+        name:null,
+        fileType:'css',
+        fileName:'12345ASDF',
+        viewType:null
+    }
+]

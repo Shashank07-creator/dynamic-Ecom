49dd47a374c377efcfdc34bbe3d103c9a880805f
Shashank <shashank_nigam@persistent.com>
2022-07-22 00:21:20 +0530

    Removed data from .gitignore to track mock data prepared

---
+++ Server/data/HeaderMock.js
@@ -0,0 +1,68 @@
+module.exports = {basic:
+    {
+    left: [
+        {
+            label: 'Image',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsmJJiw7IhylfajKNPABE1Pf24KuUqYX8f8WvzGLmsUQx7P8C49EVz3Qn1FX4vFM1Hs_E&usqp=CAU',
+            viewType: 'image'
+        },
+        {
+            label: 'Landing',
+            url: '/',
+            viewType: 'anchor'
+        },
+        {
+            label: 'Product',
+            url: '/Product',
+            viewType: 'anchor'
+        },
+        {
+            label: 'Services',
+            url: '/check',
+            viewType: 'anchor'
+        },
+        {
+            label: 'Blogs',
+            url: '/check',
+            viewType: 'anchor'
+        },
+        {
+            label: 'About US',
+            url: '/check',
+            viewType: 'anchor'
+        },
+        {
+            fileType: 'css',
+            label: '12345ASDF.css',
+            viewType: 'file'
+        }
+    ],
+    right: [
+        {
+            label: '',
+            url: '/check',
+            viewType: 'search'
+        }, 
+        {
+            label: 'Account',
+            url: '/check',
+            viewType: 'anchor'
+        },
+        {
+            label: 'Whishlist',
+            url: '/check',
+            viewType: 'anchor'
+        },
+        {
+            label: 'Cart',
+            url: '/check',
+            viewType: 'anchor'
+        },
+        {
+            label: 'Settings',
+            url: '/check',
+            viewType: 'anchor'
+        }
+    ]
+}
+}
